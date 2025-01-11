789a7e11150e8dd326a858c9e7e5c02790a87f40
YashPansare <pansareyash9740@gmail.com>
2025-01-11 22:02:58 +0530

    Add Exception & logger Files

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import sys
+
+def error_msg_details(error,error_details:sys):
+    _,_,exc_tb=error_details.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_msg="Error occured in python script name [{0}] line number [{0}] error message [{1}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+
+    return error_msg
+
+class CustomException(Exception):
+    def __init__(self, error_msg,error_details:sys):
+        super().__init__(error_msg)
+        self.error_msg=error_msg_details(error_msg,error_details=error_details)
+    
+    def __str__(self):
+        return self.error_message
+    
