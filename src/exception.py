57ac0f3b25b14a8266802e0af7236f10a70a7258
YashPansare <pansareyash9740@gmail.com>
2025-01-13 09:45:06 +0530

    Add test case for logger & Exception

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,11 @@
 import sys
+import logging
+from logger import logger
 
 def error_msg_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_msg="Error occured in python script name [{0}] line number [{0}] error message [{1}]".format(
+    error_msg="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
     file_name,exc_tb.tb_lineno,str(error))
 
     return error_msg
@@ -11,8 +13,18 @@ def error_msg_details(error,error_details:sys):
 class CustomException(Exception):
     def __init__(self, error_msg,error_details:sys):
         super().__init__(error_msg)
-        self.error_msg=error_msg_details(error_msg,error_details=error_details)
+        self.error_message=error_msg_details(error_msg,error_details=error_details)
+        logger.error(self.error_message)
     
     def __str__(self):
-        return self.error_message
-    
+         return self.error_message
+
+'''
+if __name__=="__main__":
+    logging.basicConfig(level=logging.INFO) 
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
+'''
